0d0968d487ecf81c0f0622b5b0fb7c22f4bbb02c
Eric Lobdell <eric@ericlobdell.me>
2016-09-25 18:22:56 -0400

    finished through ex 6 in ch 5

--- ch5_ex.hs
+++ ch5_ex.hs
@@ -33,7 +33,36 @@ sorted xs = and [x <= y | (x,y) <- pairs xs]
 sq100 :: Int
 sq100 = sum [ x^2 | x <- [1..100] ]
 
+-- 2 Suppose that a coordinate grid of size m x n is given by the list of all pairs (x,y)
+--   of integers such that 0 <= x <= m and 0 <= y <= n. Using a list comprehension, define
+--   a function grid :: Int -> Int -> [(Int,Int)] that returns a coordinate grid of a given size. 
 grid :: Int -> Int -> [(Int, Int)]
 grid m n = [(x, y) | x <- [0..m], y <- [0..n]]
 
+-- 3 Using a list comprehension and the grid function above, define a function 
+--   square :: Int -> [(Int,Int)] that returns a coordinate square of size n,
+--   excluding the diagonal (0,0) to (n,n).  
+square :: Int -> [(Int, Int)]
+square n = [(x,y) | (x,y) <- grid n n, x /= y ]
 
+-- 4 In a similar way top the function length, show how the library function 
+--   replicate :: Int -> a -> [a] that produces a list of identical elements
+--   can be defined using a list comprehension.
+replicate' :: Int -> a -> [a]
+replicate' n x = [x | _ <- [1..n]]
+
+-- 5 A triple (x,y,z) of positive integers is Pathagorean if it satisfies the equation
+--   x^2 + y^2 = z^2. Using a list comprehension with 3 generators, define a function 
+--   pyths :: Int -> [(Int,Int,Int)] that returns a list of such triples  whose components
+--   are at most the given limit
+
+pyths :: Int -> [(Int,Int,Int)]
+pyths n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+-- 6 A positive integer is perfect if it equals the sum of all its factors.
+--   excluding the number itself. Using a list comprehension and the function factors
+--   define a function perfects :: Int -> [Int] that returns a list of all perfect numbers
+--   up to a given limit.
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (factors x) - x == x ]
