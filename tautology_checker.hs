dc6f8696889b5808ad907446335b0a96bfd61c27
Eric Lobdell <eric@ericlobdell.me>
2017-01-31 20:06:17 -0500

    formatting

--- tautology_checker.hs
+++ tautology_checker.hs
@@ -16,7 +16,6 @@ rmdups :: Eq a => [a] -> [a]
 rmdups [] = []
 rmdups (x:xs) = x : filter (/= x) (rmdups xs)
 
-
 eval :: Substitution -> Proposition -> Bool
 eval _ (Const b)    = b
 eval s (Var x)      = find x s
@@ -25,11 +24,11 @@ eval s (And p q)    = eval s p && eval s q
 eval s (Imply p q)  = eval s p <= eval s q
 
 vars :: Proposition -> [Char]
-vars (Const _) = []
-vars (Var x) = [x]
-vars (Not p) = vars p
-vars (And p q) = vars p ++ vars q
-vars (Imply p q) = vars p ++ vars q
+vars (Const _)      = []
+vars (Var x)        = [x]
+vars (Not p)        = vars p
+vars (And p q)      = vars p ++ vars q
+vars (Imply p q)    = vars p ++ vars q
 
 bools :: Int -> [[Bool]]
 bools 0 = [[]]
@@ -43,7 +42,6 @@ substs p = map (zip vs) (bools (length vs))
 isTaut :: Proposition -> Bool
 isTaut p = and [eval s p | s <- substs p]
 
-
 p1 :: Proposition
 p1 = And (Var 'A') (Not (Var 'A'))
 
@@ -54,7 +52,7 @@ p3 :: Proposition
 p3 = Imply (Var 'A') (And (Var 'A') (Var 'B')) 
 
 p4 :: Proposition
-p4 = Imply (And (Var 'A') (Imply (Var 'A')(Var 'B'))) (Var 'B')
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
 
 
 
